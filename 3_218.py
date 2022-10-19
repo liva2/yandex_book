5e2ffc476a4ea5f533d6af7363ec4df4190f12bd
Людмила <liva2@mail.ru>
2022-10-19 20:47:09 +0700

    add file

---
+++ 3_218.py
@@ -0,0 +1,8 @@
+# ((z → y) ∧ (¬ x → w)) → ((z ≡ w) ∨ (y ∧ ¬ x)).
+print('x,y,z,w')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                if (((z <= y) and ((not x) <= w)) <= ((z == w) or (y and not x))) == 0:
+                    print(x, y, z, w)
